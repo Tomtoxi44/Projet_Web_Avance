3c2a11fd42152dc9332427e33867ec0d39c7a6db
Tommy <tommy.angibaud@gmail.com>
2024-01-30 18:38:30 +0100

    Ajout de la barre de recherche

---
+++ Frontend/src/03.globals/03.Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='h-full flex justify-center items-center flex-col rounded-lg gap-10 '>
+                <h1 className='sukui-h1-white'>Where knowledge begins</h1>
+                <input className='rounded-lg p-4 w-[75%] sukui-p-white bg-[#292927] border border-[#282826]' type="text" />
+        </div>
+    );
+};
+
+export default Home;
