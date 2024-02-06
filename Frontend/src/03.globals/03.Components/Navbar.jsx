c10123c5b96e47a6fbc441e055212d97701d7b42
J-E <beillevertj.contact@gmail.com>
2024-02-06 10:51:10 +0100

    fin pc laptop

--- Frontend/src/03.globals/03.Components/Navbar.jsx
+++ Frontend/src/03.globals/03.Components/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import logo from '../../01.assets/img/logo-perplexity.svg'
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className='bg-[#292927] h-screen w-56 absolute top-0 left-0 py-5 px-5'>
+            <p className='text-neutral-100 hover:text-cyan-400 duration-200 text-[26px] tracking-tight font-medium'>perplexity</p>
+            <input type="text" className='sukui-input' id="" />
+            <div>
+                <div className='py-3 px-4 bg-blue-400'>
+                    <p className='sukui-link'>Home</p>
+                </div>
+                <div>
+                    <p className='sukui-link'>Discover</p>
+                </div>
+                <div>
+                    <p className='sukui-link'>Library</p>
+                </div>
+            </div>
+           <p>heyehy</p> 
         </div>
     );
 };
